a071021811f19e9a86703c19d4f9bdcea86c7558
weathered57 <a_canozdogru57@hotmail.com>
2018-04-05 16:39:07 +0300

    E-ticaret

---
+++ WebAnilShop/AnilShop.Core/Interface/IRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnilShop.Core.Interface
+{
+    public interface IRepository<T>
+    {
+        List<T> List();
+
+        IQueryable<T> ListQueryable();
+
+        List<T> List(Expression<Func<T, bool>> where);
+
+        int Insert(T obj);
+
+        int Save();
+
+        int Update(T obj);
+
+
+        int Delete(T obj);
+
+
+        T Find(Expression<Func<T, bool>> where);
+
+    }
+}
